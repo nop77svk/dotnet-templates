5af8d0a4de3739725f44ca8eec915fd74558c94b
pierre@remote <nop77svk@gmail.com>
2024-07-10 13:37:37 +0200

    ➕ IsExternalInit.cs for old framework support for records

---
+++ solutions/nop77svk.solution/IsExternalInit.cs
@@ -0,0 +1,9 @@
+﻿#pragma warning disable SA1502
+// note: This somehow adds support for records to legacy .NET
+// ref: https://stackoverflow.com/a/64749403/3706181
+#if !NET5_0_OR_GREATER
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit { }
+}
+#endif
