c729e412616fa66d0ea0ffaa974fc9a0bd3293ae
pierre@remote <nop77svk@gmail.com>
2024-10-01 19:08:11 +0200

    🔧 IsExternalInit.cs fixes for analyzers

--- solutions/nop77svk.solution/IsExternalInit.cs
+++ solutions/nop77svk.solution/IsExternalInit.cs
@@ -1,4 +1,5 @@
 ﻿#pragma warning disable SA1502
+#pragma warning disable S2094
 // note: This somehow adds support for records to legacy .NET
 // ref: https://stackoverflow.com/a/64749403/3706181
 #if !NET5_0_OR_GREATER
